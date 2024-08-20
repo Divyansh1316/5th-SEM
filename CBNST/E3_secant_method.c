b111e8ddf22fac8a28dccf9d56a60d8910e66aa4
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-08-20 15:45:41 +0530

    Create E3_secant_method.c

---
+++ CBNST/E3_secant_method.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+void details()
+{
+    printf("Name: Divyansh Dhaundiyal\nSection: D(G1)\nRno: 31\n\nSecant Method\n\n");
+}
+
+float f(float x)
+{
+    float eq = (x * x * x) - (5 * x) + 1;
+    return eq;
+}
+
+void main()
+{
+    int i=0,n,flag;
+    float a, b, e;
+    float c = (a * f(b) - b * f(a)) / f(b) - f(a);
+
+    details();
+
+    printf("Enter the value of guesses (a and b) : ");
+    scanf("%f %f", &a, &b);
+    printf("Enter error : ");
+    scanf("%f", &e);
+    printf("Enter number of iterations : ");
+    scanf("%d", &n);
+
+    printf("\nA\t\tB\t\tC\n\n");
+    do
+    {
+        i++;
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+
+        printf("%f\t%f\t%f\n", a, b, c);
+
+        a=b;
+        b=c;
+
+
+
+    }while(fabs(f(c)) > e && i<n );
+    if(fabs(f(c)) <= e )
+    {
+        printf("\nFinal approximate root after %d iterations is : %f\n", i, c);
+    }
+    else
+    {
+        printf("\Approximate root not converged");
+    }
+}
